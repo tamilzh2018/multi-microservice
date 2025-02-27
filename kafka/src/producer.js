08e260578c06dc8946650a754f8a975b39532900
osboxes.org <tamizhnationalist@gmail.com>
2025-02-27 17:36:18 +0530

    kafka issue resolved

--- kafka/src/producer.js
+++ kafka/src/producer.js
@@ -1,5 +1,6 @@
 const { Kafka } = require('kafkajs');
-const kafka = new Kafka({ clientId: 'my-app', brokers: ['localhost:9092'] });
+//const kafka = new Kafka({ clientId: 'my-app', brokers: ['localhost:9092'] });
+const kafka = new Kafka({ clientId: 'my-app', brokers: ['kafka:9092'] });
 const producer = kafka.producer();
 
 const run = async () => {
--- rabbitmq/src/publisher.py
+++ rabbitmq/src/publisher.py
@@ -1,6 +1,7 @@
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+#connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 channel.basic_publish(exchange='', routing_key='hello', body='Hello RabbitMQ!')
